fcbe239d83663173c5b8aeef183fb88c33eec415
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-23 11:57:52 +0000

    Fixed first line of input file being output as well, now returns number of records in output file

--- dfp.js
+++ dfp.js
@@ -7,9 +7,7 @@ function swapColumns(line, delimiter = ';') {
 
 function limitReview(line, delimiter = ';') { //Review must be the SECOND column, Switch to limitString?, or simply use .slice(0,20)? no function?
   let elements = line.split(delimiter);
-  console.log(elements)
   let trimmedReview = [elements[0],elements[1].slice(0,20)];
-  console.log(trimmedReview)
   return trimmedReview.join(delimiter);
 }
 
@@ -27,16 +25,22 @@ function parseFile (indata, outdata, delimiter = ';') {
     removeExistingOutputFile(outdata);
     const data = fs.readFileSync(indata, 'utf-8');
     const lines = data.split(/\n/);
+    let i = 0, records = 0;
     for (let line of lines) {
-      fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter))+'\n')
+      if (i > 0) {
+        fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n')
+        records++;
+      }
+      i++;
     }
+    return records
   }
 }
 
 // Testing
 
-//parseFile('testdata.csv', 'outputtest.csv', ',')
-parseFile('datafile.csv', 'outputFile.csv', ';')
+console.log(parseFile('testdata.csv', 'outputtest.csv', ','))
+console.log(parseFile('datafile.csv', 'outputFile.csv'))
 
 // Leave this code here for the automated tests
 module.exports = {
