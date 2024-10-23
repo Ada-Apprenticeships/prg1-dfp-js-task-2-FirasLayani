a4063a73b0a0f67917aa722e798ba2e2cd757811
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-23 09:42:24 +0000

    Added functions to swap line columns and lmiting the review length

--- dfp.js
+++ dfp.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 
-function parseFile (indata, outdata, delimiter = ';') {
-
+function swapColumns(line, delimiter = ';') { //Refactor this to one line in future
+  let elements = line.split(delimiter);
+  let reversedElements = [elements[1], elements[0]];  // .reverse() ?
+  return reversedElements.join(delimiter);
 }
 
+function limitReview(line, delimiter = ';') { //Review must be the SECOND column, Switch to limitString?
+  let elements = line.split(delimiter);
+  let trimmedReview = [elements[0],elements[1].slice(0,20)];
+  return trimmedReview.join(delimiter);
+}
 
+function parseFile (indata, outdata, delimiter = ';') {
+  // IF STATEMENT TO CHECK IF INDATA EXISTS
+}
 
-  
+// Testing
+
+let lineString = 'abcdefghijklmnopqrstuvwxyz;positive';
+lineString = swapColumns(lineString)
+console.log(swapColumns(lineString))
+
+lineString = limitReview(lineString)
+console.log(limitReview(lineString))
 
 
 
