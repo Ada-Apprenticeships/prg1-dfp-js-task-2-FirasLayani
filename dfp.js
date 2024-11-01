06aa6d7e0c8af6d8a51f5ecb7b1b9beffbce7bf6
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-11-01 16:39:47 +0000

    Refactored code

--- dfp.js
+++ dfp.js
@@ -1,20 +1,15 @@
 const fs = require('fs');
-const { parse } = require('path');
 
+// Swap 2 columns in a string, separated by a delimiter
 function swapColumns(line, delimiter = ';') {
-  return line.split(delimiter).reverse().join(delimiter)
+  return line.split(delimiter).reverse().join(delimiter);
 }
 
-function limitReview(line, delimiter = ';') { //Review must be the SECOND column, Switch to limitString?, or simply use .slice(0,20)? no function?
+// Trim and limit the review to 20 characters
+function limitReview(line, delimiter = ';') { 
   let elements = line.split(delimiter);
   let trimmedReview = [elements[0].trim(),elements[1].trim().slice(0,20)];
   return trimmedReview.join(delimiter);
-}
-
-function removeExistingOutputFile(outputFile) {
-  if (fs.existsSync(outputFile)) {
-    fs.unlinkSync(outputFile);
-  }
 }
 
 function parseFile (indata, outdata, delimiter = ';') {
@@ -22,26 +17,29 @@ function parseFile (indata, outdata, delimiter = ';') {
     return -1;
 
   } else {
-    removeExistingOutputFile(outdata);
+    // Remove existing output file
+    if (fs.existsSync(outputFile)) {
+      fs.unlinkSync(outputFile);
+    }
     const data = fs.readFileSync(indata, 'utf-8');
     const lines = data.split(/\n/);
     let i = 0, records = 0;
     for (let line of lines) {
       if (i > 0) {
-        fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n')
+        fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n');
         records++;
       }
       i++;
     }
-    return records;
+
+    lines.forEach((line, rowIndex) => {
+      if(rowIndex > 0){
+      fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n');
+      }
+    })
+    return lines.length-1;
   }
 }
-
-// Testing - REMOVE IN FINAL VERSION
-
-// console.log(parseFile('testdata.csv', 'outputtest.csv', ','))
-// console.log(parseFile('datafile.csv', 'outputFile.csv'))
-// console.log(parseFile('testing/testdata_5.csv', 'testdata_1_commaoutput.csv'))
 
 // Leave this code here for the automated tests
 module.exports = {
