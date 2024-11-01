0b904ad90e46e549e3a57e7aba406ace281b7658
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-11-01 16:46:58 +0000

    Spacing fixes

--- dfp.js
+++ dfp.js
@@ -6,42 +6,36 @@ function swapColumns(line, delimiter = ';') {
 }
 
 // Trim and limit the review to 20 characters
-function limitReview(line, delimiter = ';') { 
+function limitReview(line, delimiter = ';') {
   let elements = line.split(delimiter);
-  let trimmedReview = [elements[0].trim(),elements[1].trim().slice(0,20)];
+  let trimmedReview = [elements[0].trim(), elements[1].trim().slice(0, 20)];
   return trimmedReview.join(delimiter);
 }
 
-function parseFile (indata, outdata, delimiter = ';') {
+// Read in a file, swap the columns and limit the length of the second column to 20 characters
+// Write the output to a file
+function parseFile(indata, outdata, delimiter = ';') {
   if (!fs.existsSync(indata)) {
     return -1;
-
   } else {
     // Remove existing output file
-    if (fs.existsSync(outputFile)) {
-      fs.unlinkSync(outputFile);
+    if (fs.existsSync(outdata)) {
+      fs.unlinkSync(outdata);
     }
     const data = fs.readFileSync(indata, 'utf-8');
     const lines = data.split(/\n/);
-    let i = 0, records = 0;
-    for (let line of lines) {
-      if (i > 0) {
-        fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n');
-        records++;
-      }
-      i++;
-    }
 
     lines.forEach((line, rowIndex) => {
-      if(rowIndex > 0){
-      fs.appendFileSync(outdata, limitReview(swapColumns(line,delimiter), delimiter)+'\n');
+      if (rowIndex > 0) {
+        // Swap columns and limit the review, then write to the output file
+        fs.appendFileSync(outdata, limitReview(swapColumns(line, delimiter), delimiter) + '\n');
       }
-    })
-    return lines.length-1;
+    });
+    return lines.length - 1;
   }
 }
 
 // Leave this code here for the automated tests
 module.exports = {
   parseFile,
-}
+};
