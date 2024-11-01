50cfbfe8798fddf7f2612d4088b826b11e990665
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-11-01 17:54:56 +0000

    Refactored parseFile

--- dfp.js
+++ dfp.js
@@ -1,38 +1,38 @@
 const fs = require('fs');
 
-// Swap 2 columns in a string, separated by a delimiter
+// Swap two columns in a string, separated by a delimiter
 function swapColumns(line, delimiter = ';') {
   return line.split(delimiter).reverse().join(delimiter);
 }
 
 // Trim and limit the review to 20 characters
 function limitReview(line, delimiter = ';') {
-  let elements = line.split(delimiter);
-  let trimmedReview = [elements[0].trim(), elements[1].trim().slice(0, 20)];
+  const elements = line.split(delimiter);
+  const trimmedReview = [elements[0].trim(), elements[1].trim().slice(0, 20)];
   return trimmedReview.join(delimiter);
 }
 
-// Read in a file, swap the columns and limit the length of the second column to 20 characters
-// Write the output to a file
+// Read in a file, swap columns, limit second column's length to 20 characters, and write the output to a file
 function parseFile(indata, outdata, delimiter = ';') {
+  // Check if input data exists
   if (!fs.existsSync(indata)) {
     return -1;
-  } else {
-    // Remove existing output file
-    if (fs.existsSync(outdata)) {
-      fs.unlinkSync(outdata);
-    }
-    const data = fs.readFileSync(indata, 'utf-8');
-    const lines = data.split(/\n/);
-
-    lines.forEach((line, rowIndex) => {
-      if (rowIndex > 0) {
-        // Swap columns and limit the review, then write to the output file
-        fs.appendFileSync(outdata, limitReview(swapColumns(line, delimiter), delimiter) + '\n');
-      }
-    });
-    return lines.length - 1;
   }
+
+  // Remove existing output file if it exists
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata);
+  }
+
+  const data = fs.readFileSync(indata, 'utf-8');
+  // Remove header
+  const lines = data.split(/\n/).slice(1);
+
+  lines.forEach(line => {
+    // Swap columns and limit the review, then append to the output file
+    fs.appendFileSync(outdata, limitReview(swapColumns(line, delimiter), delimiter) + '\n');
+  });
+  return lines.length - 1;
 }
 
 // Leave this code here for the automated tests
